167228f6d121d76b04908da836d9209209fb0fe7
Andrea Lopez <andreael@bu.edu>
2025-02-10 15:30:03 -0600

    home page done

---
+++ react-jobs/src/App.jsx
@@ -0,0 +1,19 @@
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import HomeCards from "./components/HomeCards"
+import JobListings from "./components/JobListings";
+import ViewAllJobs from "./components/viewAllJobs";
+
+const App = () => {
+  return (
+    <>
+      <Navbar/>
+      <Hero/>
+      <HomeCards/>
+      <JobListings/>
+      <ViewAllJobs/>
+    </>
+  );
+};
+
+export default App
